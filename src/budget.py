b89576c4d57ee636f10b825483780c834902ada8
abstudent133 <anna.bangerter@ucas-edu.net>
2026-03-30 09:52:48 -0600

    started budgeting function pseudocode

--- src/budget.py
+++ src/budget.py
@@ -1,2 +1,17 @@
 #This will contain all the necessary code for the budgeting aspect of this project
 
+#Pseudocode
+
+#budgeting function
+#parameters:user profile info
+    #show the menu of options including view, income, expense, or quit
+    #if they choose view
+        #get their infomation and input it into the graph function(made by levi)
+        #display the graph
+    #else if they choose income
+        #ask how much they got
+        #ask for the source
+        #get the time
+        #formate that info and save it to the user profile
+
+
