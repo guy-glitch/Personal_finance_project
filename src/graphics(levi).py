ba41e8c5ad973e7aeb63fc8358405241f4104fc4
flumph3927 <levi.morris@ucas-edu.net>
2026-03-30 19:12:01 +0000

    i might be done with psuedocode?

--- src/graphics(levi).py
+++ src/graphics(levi).py
@@ -1,3 +1,22 @@
 #graphs and graphics
 
-#
+#create function pie, get caption and values dictionary
+    #show pie chart with caption caption and names keys in values, portions values in values
+
+#create function plot, get plus, minus dictionaries
+    #plot line graph with plus line, minus line, and them combined line
+
+#create function bars, get values
+    #create bar chart with name values keys and values values values
+
+#create function menu, get options
+    #create window with boxes for every option
+    #return option clicked
+
+#create function inputs, get question and wrong
+    #create window, show question and text box
+    #if wrong is true, also show invalid input, try again
+    #return text box entry
+
+#create function show, get stuff
+    #create window and display stuff
